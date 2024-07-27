59e42c9d41baa7172715d154444c3e972aac7ac3
alone <154563439+neverfriendme@users.noreply.github.com>
2024-07-27 19:29:16 -0400

    Shape maker version 2 pre-release
    I have added Floats to make Shape maker fun. Shape maker is case sensitive,(Whole and Float not whole and float)

--- Shape-maker.py
+++ Shape-maker.py
@@ -1,14 +1,27 @@
 from turtle import *
-print("Shape maker V1")
+print("Shape maker v2 pre-release")
+WF = input("Whole numbers or Float numbers: ")
 
-b = int(input("Num1: ")) 
-h = int(input("Num2t: "))
-playing = True
-if playing == True:
-    while True:
-        forward(b)
-        right(h)
-        left(b)
-        right(h)
-    
-    
+if WF == "Whole":
+    Who = int(input("Who: "))
+    Who2 = int(input("Who2: "))
+    playing = True
+    if playing == True:
+        while True:
+            forward(Who)
+            right(Who2)
+            left(Who)
+            right(Who2)
+if WF == "Float":
+    Flo = float(input("Flo: "))
+    Flo2 = float(input("Flo2: "))
+    playing = True
+    if playing == True:
+        while True:
+            forward(Flo)
+            right(Flo2)
+            left(Flo)
+            right(Flo2)
+else:
+    print("Invalid")
+    quit()
