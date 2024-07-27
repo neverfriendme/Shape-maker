7582dcdd985e741409c3a3f7650bc3d4f3b3f226
alone <154563439+neverfriendme@users.noreply.github.com>
2024-07-27 13:20:13 -0400

    version 1
    version 1 can only make a limited amount of shapes

---
+++ Shape-maker.py
@@ -0,0 +1,14 @@
+from turtle import *
+print("Shape maker V1")
+
+b = int(input("Base: ")) 
+h = int(input("Height: "))
+playing = True
+if playing == True:
+    while True:
+        forward(b)
+        right(h)
+        left(b)
+        right(h)
+    
+    
